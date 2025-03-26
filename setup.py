139507691d8a33b612df288e0ee637ebd8b30985
muhammadalive <muhammadali17abc@gmail.com>
2025-03-26 16:58:43 +0500

    added new updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='click-pkg',
-    version='0.13',
+    version='0.14',
     license='MIT',
     author="Muhammadali Akbarov",
     author_email='muhammadali17abc@gmail.com',
