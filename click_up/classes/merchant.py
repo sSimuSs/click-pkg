eb77c6917907dc240f12171efefb909a1f30e6c8
Sirojiddin Mustafayev <sirojiddinmustafayev@gmail.com>
2025-05-16 21:22:37 +0500

    added MerchantApi.submit_fiscal_check() method and small code refactoring

--- click_up/classes/merchant.py
+++ click_up/classes/merchant.py
@@ -12,11 +12,9 @@ class MerchantApi:
     def __init__(self, service_id, merchant_user_id, secret_key):
         """
         Initialize the MerchantApi instance.
-
-        Args:
-            url (str): Base URL of the CLICK Merchant API.
-            merchant_user_id (str): Merchant User ID provided by CLICK.
-            secret_key (str): Secret key for authentication.
+        :param service_id: Service ID provided by CLICK.
+        :param merchant_user_id: Merchant User ID provided by CLICK.
+        :param secret_key: Secret key for authentication.
         """
         self.http = Http()
         self.secret_key = secret_key
@@ -26,30 +24,20 @@ class MerchantApi:
 
     def create_invoice(
         self,
-        amount,
-        phone_number,
-        merchant_trans_id
-    ):
+        amount: float,
+        phone_number: str,
+        merchant_trans_id: str
+    ) -> dict:
         """
         Create an invoice using the CLICK API.
-
-        Args:
-            service_id (int): Service ID.
-            amount (float): Requested amount.
-            phone_number (str): Invoice receiver.
-            merchant_trans_id (str):
-                Order ID (for online shopping) / personal account / login
-                in the billing of the supplier.
-        Returns:
-            dict: A dictionary containing the response from the API.
+        :param amount: Requested amount.
+        :param phone_number: Invoice receiver.
+        :param merchant_trans_id:
+            Order ID (for online shopping) / personal account / login
+            in the billing of the supplier.
+        :returns dict: A dictionary containing the response from the API.
         """
-        timestamp = int(time.time())
-        digest_input = f"{timestamp}{self.secret_key}"
-        digest = hashlib.sha1(digest_input.encode()).hexdigest()
-        headers = {
-            "Auth": f"{self.merchant_user_id}:{digest}:{timestamp}",
-        }
-        body = {
+        data = {
             "service_id": self.service_id,
             "amount": amount,
             "phone_number": phone_number,
@@ -57,4 +45,34 @@ class MerchantApi:
         }
 
         url = f"{self.url}/invoice/create"
-        return self.http.post(url, body, headers, 10)
+        return self._send_post_request(url, data)
+
+
+    def submit_fiscal_check(
+        self,
+        payment_id: int,
+        fiscal_check_url: str
+    ) -> dict:
+        """
+        Sends already generated fiscal check data to CLICK for linking it to the invoice
+        :param payment_id: payment ID from the CLICK side.
+        :param fiscal_check_url: URL of the fiscal check from the ofd.uz.
+        """
+
+        data = {
+            "service_id": self.service_id,
+            "payment_id": payment_id,
+            "qrcode": fiscal_check_url,
+        }
+
+        url = f"{self.url}/payment/ofd_data/submit_qrcode"
+        return self._send_post_request(url, data)
+
+    def _send_post_request(self, url: str, data: dict) -> dict:
+        timestamp = int(time.time())
+        digest_input = f"{timestamp}{self.secret_key}"
+        digest = hashlib.sha1(digest_input.encode()).hexdigest()
+        headers = {
+            "Auth": f"{self.merchant_user_id}:{digest}:{timestamp}",
+        }
+        return self.http.post(url, data, headers, 10)
